21b013a21fd855fa4aeb4528479cce186b4dc6d6
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-29 13:23:14 -0700

    image upload relavtive codes are now commented out.

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,50 +1,50 @@
-const router = require('express').Router();
-const { Image } = require('../models');
-const multer = require('multer');
+// const router = require('express').Router();
+// const { Image } = require('../models');
+// const multer = require('multer');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now(), + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now(), + file.originalname);
+//   },
+// });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
 
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
-  fileFilter: fileFilter,
-});
+// const upload = multer({
+//   storage: storage,
+//   limits: { fileSize: 1024 * 1024 * 5 },
+//   fileFilter: fileFilter,
+// });
 
-router.post(
-  '/image/uploadmulter',
-  upload.single('imageData'),
-  (req, res, next) => {
-    console.log(req.body);
-    const newImage = new Image({
-      imageName: req.body.imageName
-    });
+// router.post(
+//   '/image/uploadmulter',
+//   upload.single('imageData'),
+//   (req, res, next) => {
+//     console.log(req.body);
+//     const newImage = new Image({
+//       imageName: req.body.imageName
+//     });
 
-    newImage
-      .save()
-      .then((result) => {
-        console.log(result);
-        res.status(200).json({
-          success: true,
-          document: result,
-        });
-      })
-      .catch((err) => next(err));
-  }
-);
+//     newImage
+//       .save()
+//       .then((result) => {
+//         console.log(result);
+//         res.status(200).json({
+//           success: true,
+//           document: result,
+//         });
+//       })
+//       .catch((err) => next(err));
+//   }
+// );
 
-module.exports = router;
+// module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 router.use('/api', require('./userRoutes.js'));
 router.use('/api', require('./postRoutes.js'));
-router.use('/api', require('./imageRoutes.js'));
+// router.use('/api', require('./imageRoutes.js'));
 module.exports = router;
