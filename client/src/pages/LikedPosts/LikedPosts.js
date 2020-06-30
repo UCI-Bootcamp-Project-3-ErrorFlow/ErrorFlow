8c10f1f12250381fc37a95ca2fe18079da160f6c
Logan Morris <loganmorris96@gmail.com>
2020-06-30 11:44:03 -0700

    Added the function to filter liked posts to only see liked posts on like post page.

--- client/src/pages/LikedPosts/LikedPosts.js
+++ client/src/pages/LikedPosts/LikedPosts.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const LikedPosts = () => {
+//   const [ likeState, setLikeState] = useState({
+//     posts: []
+//   })
+
+//  useEffect(() => {
+//     axios.get(`/api/posts/${posts._id}`)
+//       .then(({ data }) => { 
+//         setLikeState({ ...likeState, posts: data })
+//       .then(({ data }) => {
+//         const likeFiltered = data.filter( like => like.isLiked === true )
+//         setLikeState({ ...likeState, posts: likeFiltered})
+//       })
+//       .catch(err => console.log(err))
+//       })
+    
+//       .catch(err => console.error(err))
+//   }, [])
+
   return (
     <>
-      <h1>Liked posts page</h1>
+    <h1>This is the Liked Page</h1>
+    {
+      // likeState.likes.map(like => (
+      // <div title={likes.title}>Liked post
+      // <h3>
+      //   {likes.title}
+      //   {likes.tag}
+      //   {likes.commentBody}
+      //   {likes.commentAuthor}
+      //   {likes.likeValue}
+      //   {likes.isLiked}
+      //   {likes.author}
+      // </h3>
+      // </div>
+
+      // ))
+}
     </>
-  );
+  )
 };
 
 export default LikedPosts;
