83cdbcb7c7d0119bdb8d52851af2bc097fe2182a
acerjak <62491401+acerjak@users.noreply.github.com>
2020-07-06 14:00:26 -0700

    basic layout added to App.js from antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
+import 'antd/dist/antd.css'
+import { Layout, Menu } from 'antd';
+import { Button } from 'antd';
 import { Switch, Link, Route, BrowserRouter as Router } from 'react-router-dom';
 import Main from './pages/Main';
 import SignUp from './pages/SignUp';
 import LikedPosts from './pages/LikedPosts';
 import MyPosts from './pages/MyPosts';
 import NewPosts from './pages/NewPosts';
-
-import Join from './components/Join';
-import Chat from './components/Chat';
-
+// import Join from './components/Join';
+// import Chat from './components/Chat';
 import './App.css';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   const [postState] = useState({});
@@ -22,17 +25,19 @@ function App() {
   return (
     <>
       <Router>
-        <div>
-          <nav>
-            <Link to='/'>Sign Up</Link>
-            <Link to='/Main'>Main</Link>
-            <Link to='/LikedPosts'>Liked Post</Link>
-            <Link to='/MyPosts'>My Posts</Link>
-            <Link to='/NewPosts'>New Posts</Link>
-            <Link to='/join'>Join chat</Link>
-            <Link to='/chat'></Link>
-            <button onClick={postState.signOutBtn}>Sign Out</button>
-          </nav>
+        <div classname="container">
+          <Layout>
+            <Header className="navBar">
+                <Button className="navBtns"><Link to='/'>Sign Up</Link></Button>
+                <Button className="navBtns"><Link to='/Main'>Main</Link></Button>
+            {/* <Link to='/LikedPosts'>Liked Post</Link> */}
+                <Button className="navBtns"><Link to='/MyPosts'>My Posts</Link></Button>
+                <Button className="navBtns"><Link to='/NewPosts'>New Posts</Link></Button>
+            {/* <Link to='/join'>Join chat</Link>
+            <Link to='/chat'></Link> */}
+                <Button className="navBtns" onClick={postState.signOutBtn}>Sign Out</Button>
+                <h2 className="navHeader">ErrorFlow</h2>
+                </Header>
           <Switch>
             <Route exact path='/'>
               <SignUp />
@@ -49,14 +54,14 @@ function App() {
             <Route path='/NewPosts'>
               <NewPosts />
             </Route>
-            <Route path='/join'>
+            {/* <Route path='/join'>
               <Join />
             </Route>
             <Route path='/chat' component={Chat}>
-              
-            </Route>
+            </Route> */}
             <Route path='/'> 404 Page Not Found</Route>
           </Switch>
+          </Layout>
         </div>
       </Router>
     </>
