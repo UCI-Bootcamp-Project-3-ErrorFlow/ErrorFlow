de57e3288ee0e607f773ebb708bbdbf3d8bf8a05
acerjak <62491401+acerjak@users.noreply.github.com>
2020-07-05 22:04:37 -0700

    updated Comments model

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,13 @@
 const { model, Schema } = require('mongoose');
 
 const Comment = new Schema({
-  title: String,
+  // title: String,
   commentBody: String,
   postId:{
     type: Schema.Types.ObjectId,
     ref: 'Post'
   },
-  commentAuthor: {
-    type: Schema.Types.ObjectId, //we are building relations here with model called 'User'.
-    ref: 'User', //will be populated in post routes
-  },
+  commentAuthor: String
 });
 
 module.exports = model('Comment', Comment);
