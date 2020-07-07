a35fd008629dd3250cab53504cd7ab034a20a791
Logan Morris <loganmorris96@gmail.com>
2020-07-07 10:23:01 -0700

    Uncommented messages.js and messages.js

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import './Message.css';
-// import ReactEmoji from 'react-emoji';
+import React from 'react';
+import './Message.css';
+import ReactEmoji from 'react-emoji';
 
-// const Message = ({ message: { text, user }, name }) => {
-//   let isSentByCurrentUser = false;
-//   const trimmedName = name.trim().toLowerCase();
+const Message = ({ message: { text, user }, name }) => {
+  let isSentByCurrentUser = false;
+  const trimmedName = name.trim().toLowerCase();
 
-//   if (user === trimmedName) {
-//     isSentByCurrentUser = true;
-//   }
+  if (user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
 
-//   return isSentByCurrentUser ? (
-//     <div className='messageCotainer'>
-//       <p className='sentText'>{trimmedName}</p>
-//       <div className='messageBox'>
-//         <p className='messageText colorWhite'>{ReactEmoji.emojify(text)}</p>
-//       </div>
-//     </div>
-//   ) : (
-//     <div className='messageCotainer justriftStart'>
-//       <div className='messageBox backgroundLight'>
-//         <p className='messageText colorDark'>{ReactEmoji.emojify(text)}</p>
-//       </div>
-//       <p className='sentText pl-10'>{user}</p>
-//     </div>
-//   );
-// };
+  return isSentByCurrentUser ? (
+    <div className='messageCotainer'>
+      <p className='sentText'>{trimmedName}</p>
+      <div className='messageBox'>
+        <p className='messageText colorWhite'>{ReactEmoji.emojify(text)}</p>
+      </div>
+    </div>
+  ) : (
+    <div className='messageCotainer justriftStart'>
+      <div className='messageBox backgroundLight'>
+        <p className='messageText colorDark'>{ReactEmoji.emojify(text)}</p>
+      </div>
+      <p className='sentText pl-10'>{user}</p>
+    </div>
+  );
+};
 
-// export default Message;
+export default Message;
